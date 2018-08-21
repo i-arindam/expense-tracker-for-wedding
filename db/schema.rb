669c2560a1e2735f0900b2cf4aed7e52c44612d9
Arindam Chakraborty <arismart99@gmail.com>
2018-08-21 16:32:51 +0530

    add migration to create users

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180821110046) do
+
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "email", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "gender", limit: 1
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
