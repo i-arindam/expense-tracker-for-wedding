609a5f5c849fe34742c8846384ce561b3a6bbed4
Arindam Chakraborty <arismart99@gmail.com>
2018-08-21 17:43:54 +0530

    add login/logout/signup functions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,22 @@
 class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id.to_s
+      redirect_to root_path, notice: 'Successfully logged in'
+    else
+      flash[:alert] = 'Email/password invalid, try again'
+      render :new
+    end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    redirect_to login_path, notice: 'Logged out'
+  end
 end
